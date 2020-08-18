4c5e1a2df5ad3f2f64434acf874883896ce88e7b
Henry Solomon <sehenrysolomon@outlook.com>
2020-08-18 14:20:05 -0700

    Methods to hash password

--- application/auth/models.py
+++ application/auth/models.py
@@ -1,4 +1,5 @@
 from application import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model):
@@ -7,6 +8,14 @@ class User(db.Model):
     email = db.Column(db.String(68), index=True, unique=True)
     username = db.Column(db.String(68), index=True, unique=True)
     password_hash = db.Column(db.String(80), index=True, unique=True)
+
+    def set_password(self, password):
+
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
 
