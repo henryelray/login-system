fc6dc6ddde88db401d7cae621feb9be0031cafb5
Henry Solomon <sehenrysolomon@outlook.com>
2020-08-18 10:43:52 -0700

    Added classes for Login Form

---
+++ application/auth/forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, SubmitField, StringField, BooleanField
+from wtforms.validators import ValidationError, DataRequired
+
+
+class LoginForm(FlaskForm):
+
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Log In')
