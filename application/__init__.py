2bd1a0169455c8afa917073f137cd11b45401a53
Henry Solomon <sehenrysolomon@outlook.com>
2020-08-18 16:37:13 -0700

    reset password link

--- application/__init__.py
+++ application/__init__.py
@@ -8,6 +8,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
 
+from application.auth import models
 from application.auth.views import login_bp
 
 app.register_blueprint(login_bp)
