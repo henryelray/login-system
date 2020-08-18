bd8d3dabe26cd72cb4f4ca6702d844da3ca88b00
Henry Solomon <sehenrysolomon@outlook.com>
2020-08-18 13:45:35 -0700

    Added email field

--- application/auth/models.py
+++ application/auth/models.py
@@ -4,6 +4,7 @@ from application import db
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, unique=True)
+    email = db.Column(db.String(68), index=True, unique=True)
     username = db.Column(db.String(68), index=True, unique=True)
     password_hash = db.Column(db.String(80), index=True, unique=True)
 
