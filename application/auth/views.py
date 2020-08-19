5534bc9c4f6a84fb2d04df56cd14e13fa524ea38
Henry Solomon <sehenrysolomon@outlook.com>
2020-08-19 16:58:10 -0700

    Logout view added

--- application/auth/views.py
+++ application/auth/views.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, url_for, flash
 from application.auth import login_bp
 from application.auth.forms import LoginForm
 from application.auth.models import User
-from flask_login import login_user, current_user
+from flask_login import login_user, current_user, logout_user
 
 
 @login_bp.route('/index')
@@ -10,21 +10,28 @@ def index():
     return render_template('index.html')
 
 
+@login_bp.route('/login')
 @login_bp.route('/', methods=['GET', 'POST'])
 def login():
 
     if current_user.is_authenticated:
-
         return redirect(url_for('login_bp.index'))
+
     form = LoginForm()
 
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            return redirect(url_for('login_bp.index'))
+            return redirect(url_for('login_bp.login'))
         login_user(user, remember=form.remember_me.data)
 
         return redirect(url_for('login_bp.index'))
 
     return render_template('auth/login.html', form=form)
+
+
+@login_bp.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login_bp.login'))
